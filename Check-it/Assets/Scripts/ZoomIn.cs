ac8d2b20cf0ee7a71d5b0151438a56b6fc6a0db1
Adrian Cantu <adriancantu1407@gmail.com>
2023-04-20 17:10:18 -0600

    Nuevo Carro e inicio de zoom para llantas

---
+++ Check-it/Assets/Scripts/ZoomIn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomIn : MonoBehaviour
+{
+    public Camera tireCamera;
+    public Camera mainCamera;
+    
+
+    private void Start()
+    {
+        // Disable the tire camera initially
+        tireCamera.enabled = false;
+    }
+
+    public void OnClick()
+    {
+        // Disable the main camera
+        mainCamera.enabled = false;
+
+        // Enable the tire camera
+        tireCamera.enabled = true;
+    }
+
+    public void OnBackButtonClicked()
+    {
+        Debug.Log("Back button clicked!");
+    
+        // Disable the tire camera
+        tireCamera.enabled = false;
+
+        // Enable the main camera
+        mainCamera.enabled = true;
+    }
+}
