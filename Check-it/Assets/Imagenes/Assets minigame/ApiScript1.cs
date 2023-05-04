6accfe9b99229782cd6dfd5dc6d876d3083d28e9
JosueSosa01 <117098057+JosueSosa01@users.noreply.github.com>
2023-05-04 13:04:24 -0600

    Botones api

---
+++ Check-it/Assets/Imagenes/Assets minigame/ApiScript1.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ApiScript1 : MonoBehaviour {
+    public string apiUrl = "http://presno2112.pythonanywhere.com/api/results/";
+
+    IEnumerator Start () {
+        using (UnityWebRequest request = UnityWebRequest.Get(apiUrl)) {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success) {
+                Debug.Log(request.error);
+            } else {
+                string json = request.downloadHandler.text;
+                ParseJson(json);
+            }
+        }
+    }
+
+   void ParseJson (string json) {
+    Debug.Log(json); // Imprime el JSON recibido en la consola
+    // Parsea el JSON y muestra el mensaje en la consola
+    MessageData message = JsonUtility.FromJson<MessageData>(json);
+    Debug.Log(message.message);
+}
+
+}
+
+[System.Serializable]
+public class MessageData {
+    public string message;
+}
+
